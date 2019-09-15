ca7f7742aad3a803581c15d47c59e92298a597a5
Cipi <ciobanuciprian0000@gmail,com>
2019-09-15 05:12:04 -0700

    can register as a normal user or as an admin. can add member if admin dont see the add member option if normal user

--- EventCalendar/app/src/main/java/com/ciprian/eventcalendar/Register.java
+++ EventCalendar/app/src/main/java/com/ciprian/eventcalendar/Register.java
@@ -1,15 +1,19 @@
 package com.ciprian.eventcalendar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,15 +23,21 @@ import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 
+import static android.view.View.GONE;
+
 public class Register extends AppCompatActivity {
 
-    EditText etName, etPassword, etPasswordRepeat;
+    EditText etName, etPassword, etPasswordRepeat, etPasswordAdmin;
     Button btnRegister;
+    CheckBox cbAdmin;
     TextView tvMail;
 
     private View mProgressView;
     private View mLoginFormView;
     private TextView tvLoad;
+
+    private String isAdminText = "";
+    private boolean requiresAdminPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,23 @@ public class Register extends AppCompatActivity {
         mProgressView = findViewById(R.id.login_progress);
         mLoginFormView = findViewById(R.id.login_form);
         tvLoad = findViewById(R.id.tvLoad);
+        cbAdmin = findViewById(R.id.cbAdmin);
+        etPasswordAdmin = findViewById(R.id.etPasswordAdmin);
+
+        etPasswordAdmin.setVisibility(GONE);
+
+        cbAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etPasswordAdmin.getVisibility() == View.VISIBLE) {
+                    etPasswordAdmin.setVisibility(GONE);
+                    requiresAdminPassword = false;
+                } else {
+                    etPasswordAdmin.setVisibility(View.VISIBLE);
+                    requiresAdminPassword = true;
+                }
+            }
+        });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,40 +86,94 @@ public class Register extends AppCompatActivity {
                         String name = etName.getText().toString().trim();
                         String email = tvMail.getText().toString().trim();
                         String password = etPassword.getText().toString().trim();
+                        boolean isAdmin = false;
 
-                        BackendlessUser user = new BackendlessUser();
-                        user.setEmail(email);
-                        user.setPassword(password);
-                        user.setProperty("name", name);
-
-                        final Member member = new Member();
-                        member.setName(name);
-
-                        Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
-                            @Override
-                            public void handleResponse(BackendlessUser response) {
-                                Toast.makeText(Register.this, "New user registered", Toast.LENGTH_SHORT).show();
-                                Backendless.Persistence.save(member, new AsyncCallback<Member>() {
-                                    @Override
-                                    public void handleResponse(Member response) {
-                                        Toast.makeText(Register.this, "Member saved", Toast.LENGTH_SHORT).show();
-                                    }
-
-                                    @Override
-                                    public void handleFault(BackendlessFault fault) {
-                                        Toast.makeText(Register.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                                Register.this.finish();
-                                startActivity(new Intent(Register.this, Login.class));
-                            }
-
-                            @Override
-                            public void handleFault(BackendlessFault fault) {
-                                Toast.makeText(Register.this, "Error:" + fault.getMessage(), Toast.LENGTH_SHORT).show();
+                        if (requiresAdminPassword) {
+                            if (etPasswordAdmin.getText().toString().isEmpty()) {
+                                Toast.makeText(Register.this, "Please enter a 6 digit password", Toast.LENGTH_SHORT).show();
                                 showProgress(false);
+                            } else {
+                                if (etPasswordAdmin.getText().toString().trim().equals("123456")) {
+                                    Toast.makeText(Register.this, "You are an admin", Toast.LENGTH_SHORT).show();
+                                    isAdmin = true;
+                                    BackendlessUser user = new BackendlessUser();
+                                    user.setProperty("admin", isAdmin);
+                                    user.setEmail(email);
+                                    user.setPassword(password);
+                                    user.setProperty("name", name);
+
+                                    final Member member = new Member();
+                                    member.setName(name);
+
+                                    Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
+                                        @Override
+                                        public void handleResponse(BackendlessUser response) {
+                                            Toast.makeText(Register.this, "New user registered", Toast.LENGTH_SHORT).show();
+                                            Backendless.Persistence.save(member, new AsyncCallback<Member>() {
+                                                @Override
+                                                public void handleResponse(Member response) {
+                                                    Toast.makeText(Register.this, "Member saved", Toast.LENGTH_SHORT).show();
+                                                }
+
+                                                @Override
+                                                public void handleFault(BackendlessFault fault) {
+                                                    Toast.makeText(Register.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                            Register.this.finish();
+                                            startActivity(new Intent(Register.this, Login.class));
+                                        }
+
+                                        @Override
+                                        public void handleFault(BackendlessFault fault) {
+                                            Toast.makeText(Register.this, "Error:" + fault.getMessage(), Toast.LENGTH_SHORT).show();
+                                            showProgress(false);
+                                        }
+                                    });
+                                } else {
+                                    Toast.makeText(Register.this, "6 digit code incorrect", Toast.LENGTH_SHORT).show();
+                                    showProgress(false);
+                                }
                             }
-                        });
+                        } else {
+                            BackendlessUser user = new BackendlessUser();
+                            user.setProperty("admin", isAdmin);
+                            user.setEmail(email);
+                            user.setPassword(password);
+                            user.setProperty("name", name);
+
+                            final Member member = new Member();
+                            member.setName(name);
+
+                            Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
+                                @Override
+                                public void handleResponse(BackendlessUser response) {
+                                    Toast.makeText(Register.this, "New user registered", Toast.LENGTH_SHORT).show();
+                                    Backendless.Persistence.save(member, new AsyncCallback<Member>() {
+                                        @Override
+                                        public void handleResponse(Member response) {
+                                            Toast.makeText(Register.this, "Member saved", Toast.LENGTH_SHORT).show();
+                                        }
+
+                                        @Override
+                                        public void handleFault(BackendlessFault fault) {
+                                            Toast.makeText(Register.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                    Register.this.finish();
+                                    startActivity(new Intent(Register.this, Login.class));
+                                }
+
+                                @Override
+                                public void handleFault(BackendlessFault fault) {
+                                    Toast.makeText(Register.this, "Error:" + fault.getMessage(), Toast.LENGTH_SHORT).show();
+                                    showProgress(false);
+                                }
+                            });
+
+                        }
+
+
                     } else {
                         Toast.makeText(Register.this, "Passwords are different", Toast.LENGTH_SHORT).show();
                     }
@@ -108,36 +189,36 @@ public class Register extends AppCompatActivity {
     private void showProgress(final boolean show) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.setVisibility(show ? GONE : View.VISIBLE);
             mLoginFormView.animate().setDuration(shortAnimTime).alpha(
                     show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                    mLoginFormView.setVisibility(show ? GONE : View.VISIBLE);
                 }
             });
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.setVisibility(show ? View.VISIBLE : GONE);
             mProgressView.animate().setDuration(shortAnimTime).alpha(
                     show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                    mProgressView.setVisibility(show ? View.VISIBLE : GONE);
                 }
             });
-            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.setVisibility(show ? View.VISIBLE : GONE);
             tvLoad.animate().setDuration(shortAnimTime).alpha(
                     show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+                    tvLoad.setVisibility(show ? View.VISIBLE : GONE);
                 }
             });
         } else {
 // The ViewPropertyAnimator APIs are not available, so simply show
 // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mProgressView.setVisibility(show ? View.VISIBLE : GONE);
+            tvLoad.setVisibility(show ? View.VISIBLE : GONE);
+            mLoginFormView.setVisibility(show ? GONE : View.VISIBLE);
         }
     }
 }
